e1b440c9c7b21e2787bc9f1ceaefcc5194b47cbd
Sean Foo <seanfoo73@gmail.com>
2014-06-20 13:47:16 -0700

    tic tac toe project

---
+++ TicTacToeBase/tictactoe.js
@@ -0,0 +1,27 @@
+/*------------------------------
+	changes the message displayed by the game
+------------------------------*/
+function setMessage( msgTxt )
+{
+}
+
+/*------------------------------
+	This will be called when the js file loads
+------------------------------*/
+function setUpSquares() 
+{
+}
+
+/*------------------------------
+	handleClick will fill the square in with a letter
+------------------------------*/
+function handleClick(event)
+{
+}
+
+/*------------------------------
+	This will be called when the js file loads
+------------------------------*/
+$(function () 
+{
+} );
